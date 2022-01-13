bf223e056e0642f5f4d2236b8bdad70dfb4bb620
Dan Jiang <danjiang5956@gmail.com>
2022-01-13 20:16:16 +0800

    Hello Triangle

--- FirstOpenGL/main.cpp
+++ FirstOpenGL/main.cpp
@@ -47,7 +47,7 @@ int main()
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
+		std::cout << " Failed to initialize GLAD " << std::endl;
 		return -1;
 	}
 
